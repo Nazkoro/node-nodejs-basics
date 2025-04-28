0c02b0a47095440cc9155a66e918a4016ac71cf7
nazkoro <rusetskiy2788@mail.ru>
2025-04-28 14:58:55 +0300

    feat: add logic for parseArgs func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arg = process.argv.slice(2)
+    let result = ''
+    arg.forEach((item, i) => {
+        if(i % 2 === 0){
+            result = result + `${item.slice(2)} is ${arg[i + 1]}, `
+        }
+    })
+    console.log(result)
 };
 
 parseArgs();
