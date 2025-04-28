574a9903849752549a13312f89c234a05927c56e
nazkoro <rusetskiy2788@mail.ru>
2025-04-28 15:54:25 +0300

    feat: rafactor, use mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,17 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c.cjs');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import './files/c.cjs'; 
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = JSON.parse(readFileSync(new URL('./files/a.json', import.meta.url)));
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = JSON.parse(readFileSync(new URL('./files/b.json', import.meta.url)));
 }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -33,8 +37,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
-
