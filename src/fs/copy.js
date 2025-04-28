302f6f5287a556ebe993355c738328ed74d5abcf
nazkoro <rusetskiy2788@mail.ru>
2025-04-28 12:39:29 +0300

    feat: add logic for copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const tagetSourceDirName = path.join(dirname, 'files');
+const tagetDistDirName = path.join(dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.access(tagetSourceDirName)
+        await fs.cp(tagetSourceDirName, tagetDistDirName, {recursive: true  , errorOnExist: true, force: false})
+       
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await copy();
