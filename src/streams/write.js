a160952ff6b666cc80b5148dc784893664cddf1b
nazkoro <rusetskiy2788@mail.ru>
2025-04-28 22:22:24 +0300

    feat : add logic for stream  functions tranform and write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'stream';
 const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream(new URL('./files/fileToWrite.txt', import.meta.url), 'utf8');
+
+    console.log('Введите текст (для выхода Ctrl+С)');
+    
+         pipeline(
+            process.stdin,
+            writableStream,
+            (err)=>{}
+        );
+    
 };
 
 await write();
