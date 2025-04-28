b00df740e3e8e0b253b4c1e9260f5aab0ad2713d
nazkoro <rusetskiy2788@mail.ru>
2025-04-28 14:24:11 +0300

    feat: add logic for parseEnv func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+const env = process.env;
+const result = {};
+
+for (const key in env) {
+  if (key.startsWith('RSS_')) {
+    result[key] = env[key];
+  }
+}
+
+const output = Object.entries(result).map(([key, value]) => `${key}=${value}`).join('; ');
+  console.log(output);
 };
 
 parseEnv();
